4611e81b74edd03f98e406c928a7e99fa1a3c89b
lce <xx@xx.com>
2019-03-30 09:41:30 +0800

    上交前提交

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 //导入所需插件 
-const gulp = require('gulp');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const concat = require('gulp-concat');
-const babel = require('gulp-babel');
-const cssmin = require('gulp-cssmin');
-const imagemin = require('gulp-imagemin');
-const sass = require('gulp-sass');
+const gulp = require('gulp'); 
 
+const rename = require('gulp-rename'); //重命名{需要参数}
+const concat = require('gulp-concat');//合并文件 需要参数
+
+const cssmin = require('gulp-cssmin');//压缩css
+const imagemin = require('gulp-imagemin');//压缩图片
+const sass = require('gulp-sass');//scss文件转css
+const uglify = require('gulp-uglify'); //压缩js文件
 
 
 	 
@@ -22,13 +22,29 @@ gulp.task("sassmin",function(){
 gulp.task("babelUglify",function  () {
 	gulp.src("./src/Es6js/*.js")
 	.pipe(babel({
-		presets:["@babel/env"]
+		presets:[es2015]
 	}))
 	.pipe(uglify())
 	.pipe(rename({"suffix":".min"}))
 	.pipe(gulp.dest("./dest/Es5js"))
 })
+//gulp.task("toes5",function(){
+//	gulp.src("./src/Es6js/*.js").pipe( babel() ).pipe( gulp.dest("dest/Es5js") )
+//})
+//gulp.task("babelUglify",function(){
+//	gulp.src("./src/Es6js/*.js").pipe(babel({
+//		presets:[es2015]
+//	})).pipe( uglify() )
+//								.pipe( rename({"suffix":".min"})
+//								.pipe(gulp.dest("dest/Es5js")) )
+//})
+gulp.task("imagemin",function(){
+	gulp.src("./src/image/*.*")
+	.pipe(imagemin())
+	.pipe(rename({"suffix":".min"}))
+	.pipe(gulp.dest("./dest/images"))
+})
 
  gulp.task("watch",function  () {
- 	gulp.watch(["./src/scss/*.scss","./src/Es6js/*.js"],["sassmin","babelUglify"])
+ 	gulp.watch(["./src/scss/*.scss","./src/Es6js/*.js"],["sassmin","babelUglify","imagemin"])
  })
